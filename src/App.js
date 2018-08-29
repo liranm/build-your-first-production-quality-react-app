1177496640ff5063de720ed6644da4d2f43a54d9
Liran Mery <liranm@ironsrc.com>
2018-08-29 09:05:06 +0300

    Create a Stateless Functional Component for an Input Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TodoForm } from './components/todo/TodoForm';
 
 class App extends Component {
   constructor() {
@@ -31,9 +32,10 @@ class App extends Component {
           <h1 className="App-title">React Todos</h1>
         </header>
         <div className="Todo-App">
-          <form>
-            <input type="text" value={this.state.currentTodo} onChange={this.handleInputChange} />
-          </form>
+          <TodoForm 
+            handleInputChange={this.handleInputChange}
+            currentTodo={this.state.currentTodo} 
+          />
           <div className="Todo-List">
             <ul>
               {this.state.todos.map(todo => 
